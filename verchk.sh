a0cbdb31a440b0ff7b4e147480bf07e0f299bd1d
Stephane Fontaine <bsfc1685@gmail.com>
2019-05-15 19:21:38 -0400

    ddrescue: initial implementation

--- verchk.sh
+++ verchk.sh
@@ -41,6 +41,12 @@ cpio)
 		sed '/latest/d;s/href=//g;s/"//g' |sort -V |tail -n1
 	rm /tmp/verchk-$MODULE.html
 	;;
+ddrescue)
+	wget -q $GNU_URL/$MODULE -O /tmp/verchk-$MODULE.html &&
+	grep -o 'href="'$MODULE'-[0-9]*.*lz"' /tmp/verchk-$MODULE.html |
+		sed 's/href=//g;s/"//g' |sort -V |tail -n1
+	rm /tmp/verchk-$MODULE.html
+	;;
 -V|--version)
 	echo "verchk.sh $VERCHK_VERSION" && exit 0
 	;;
