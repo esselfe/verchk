93f2caef932d862ccc9341dc3e3fa959094eaa05
Stephane Fontaine <bsfc1685@gmail.com>
2019-05-15 20:27:08 -0400

    0.0.1.2: rewrite calling scheme

--- verchk.sh
+++ verchk.sh
@@ -1,129 +1,37 @@
-#!/bin/bash
 
-VERCHK_VERSION="0.0.1.1"
+
+VERCHK_VERSION="0.0.1.2"
 MODULE="$1"
 GNU_URL=http://gnu.mirror.iweb.com/
 
+verchk() {
+	wget -q $GNU_URL/$MODULE -O /tmp/verchk-$MODULE.html &&
+	grep -o 'href="'$MODULE'-[0-9]*.*'$1'"' /tmp/verchk-$MODULE.html |
+		sed '/latest/d;s/href=//g;s/"//g' |sort -V |tail -n1
+	rm /tmp/verchk-$MODULE.html
+}
+
 case "$MODULE" in
-autoconf)
-	wget -q $GNU_URL/$MODULE -O /tmp/verchk-$MODULE.html &&
-	grep -o 'href="'$MODULE'-[0-9]*.*xz"' /tmp/verchk-$MODULE.html |
-		sed '/latest/d;s/href=//g;s/"//g' |sort -V |tail -n1
-	rm /tmp/verchk-$MODULE.html
-	;;
-automake)
-	wget -q $GNU_URL/$MODULE -O /tmp/verchk-$MODULE.html &&
-	grep -o 'href="'$MODULE'-[0-9]*.*xz"' /tmp/verchk-$MODULE.html |
-		sed 's/href=//g;s/"//g' |sort -V |tail -n1
-	rm /tmp/verchk-$MODULE.html
-	;;
-bc)
-	wget -q $GNU_URL/$MODULE -O /tmp/verchk-$MODULE.html &&
-	grep -o 'href="'$MODULE'-[0-9]*.*gz"' /tmp/verchk-$MODULE.html |
-		sed 's/href=//g;s/"//g' |sort -V |tail -n1
-	rm /tmp/verchk-$MODULE.html
-	;;
-bison)
-	wget -q $GNU_URL/$MODULE -O /tmp/verchk-$MODULE.html &&
-	grep -o 'href="'$MODULE'-[0-9]*.*xz"' /tmp/verchk-$MODULE.html |
-		sed 's/href=//g;s/"//g' |sort -V |tail -n1
-	rm /tmp/verchk-$MODULE.html
-	;;
-coreutils)
-	wget -q $GNU_URL/$MODULE -O /tmp/verchk-$MODULE.html &&
-	grep -o 'href="'$MODULE'-[0-9]*.*xz"' /tmp/verchk-$MODULE.html |
-		sed 's/href=//g;s/"//g' |sort -V |tail -n1
-	rm /tmp/verchk-$MODULE.html
-	;;
-cpio)
-	wget -q $GNU_URL/$MODULE -O /tmp/verchk-$MODULE.html &&
-	grep -o 'href="'$MODULE'-[0-9]*.*bz2"' /tmp/verchk-$MODULE.html |
-		sed '/latest/d;s/href=//g;s/"//g' |sort -V |tail -n1
-	rm /tmp/verchk-$MODULE.html
-	;;
-ddrescue)
-	wget -q $GNU_URL/$MODULE -O /tmp/verchk-$MODULE.html &&
-	grep -o 'href="'$MODULE'-[0-9]*.*lz"' /tmp/verchk-$MODULE.html |
-		sed 's/href=//g;s/"//g' |sort -V |tail -n1
-	rm /tmp/verchk-$MODULE.html
-	;;
-diffutils)
-	wget -q $GNU_URL/$MODULE -O /tmp/verchk-$MODULE.html &&
-	grep -o 'href="'$MODULE'-[0-9]*.*xz"' /tmp/verchk-$MODULE.html |
-		sed 's/href=//g;s/"//g' |sort -V |tail -n1
-	rm /tmp/verchk-$MODULE.html
-	;;
-ed)
-	wget -q $GNU_URL/$MODULE -O /tmp/verchk-$MODULE.html &&
-	grep -o 'href="'$MODULE'-[0-9]*.*lz"' /tmp/verchk-$MODULE.html |
-		sed 's/href=//g;s/"//g' |sort -V |tail -n1
-	rm /tmp/verchk-$MODULE.html
-	;;
-fdisk)
-	wget -q $GNU_URL/$MODULE -O /tmp/verchk-$MODULE.html &&
-	grep -o 'href="'$MODULE'-[0-9]*.*bz2"' /tmp/verchk-$MODULE.html |
-		sed 's/href=//g;s/"//g' |sort -V |tail -n1
-	rm /tmp/verchk-$MODULE.html
-	;;
-findutils)
-	wget -q $GNU_URL/$MODULE -O /tmp/verchk-$MODULE.html &&
-	grep -o 'href="'$MODULE'-[0-9]*.*gz"' /tmp/verchk-$MODULE.html |
-		sed 's/href=//g;s/"//g' |sort -V |tail -n1
-	rm /tmp/verchk-$MODULE.html
-	;;
-fontutils)
-	wget -q $GNU_URL/$MODULE -O /tmp/verchk-$MODULE.html &&
-	grep -o 'href="'$MODULE'-[0-9]*.*gz"' /tmp/verchk-$MODULE.html |
-		sed 's/href=//g;s/"//g' |sort -V |tail -n1
-	rm /tmp/verchk-$MODULE.html
-	;;
-gawk)
-	wget -q $GNU_URL/$MODULE -O /tmp/verchk-$MODULE.html &&
-	grep -o 'href="'$MODULE'-[0-9]*.*xz"' /tmp/verchk-$MODULE.html |
-		sed 's/href=//g;s/"//g' |sort -V |tail -n1
-	rm /tmp/verchk-$MODULE.html
-	;;
-gdb)
-	wget -q $GNU_URL/$MODULE -O /tmp/verchk-$MODULE.html &&
-	grep -o 'href="'$MODULE'-[0-9]*.*xz"' /tmp/verchk-$MODULE.html |
-		sed 's/href=//g;s/"//g' |sort -V |tail -n1
-	rm /tmp/verchk-$MODULE.html
-	;;
-gdbm)
-	wget -q $GNU_URL/$MODULE -O /tmp/verchk-$MODULE.html &&
-	grep -o 'href="'$MODULE'-[0-9]*.*gz"' /tmp/verchk-$MODULE.html |
-		sed '/latest/d;s/href=//g;s/"//g' |sort -V |tail -n1
-	rm /tmp/verchk-$MODULE.html
-	;;
-gettext)
-	wget -q $GNU_URL/$MODULE -O /tmp/verchk-$MODULE.html &&
-	grep -o 'href="'$MODULE'-[0-9]*.*xz"' /tmp/verchk-$MODULE.html |
-		sed 's/href=//g;s/"//g' |sort -V |tail -n1
-	rm /tmp/verchk-$MODULE.html
-	;;
-gmp)
-	wget -q $GNU_URL/$MODULE -O /tmp/verchk-$MODULE.html &&
-	grep -o 'href="'$MODULE'-[0-9]*.*xz"' /tmp/verchk-$MODULE.html |
-		sed 's/href=//g;s/"//g' |sort -V |tail -n1
-	rm /tmp/verchk-$MODULE.html
-	;;
-grep)
-	wget -q $GNU_URL/$MODULE -O /tmp/verchk-$MODULE.html &&
-	grep -o 'href="'$MODULE'-[0-9]*.*xz"' /tmp/verchk-$MODULE.html |
-		sed 's/href=//g;s/"//g' |sort -V |tail -n1
-	rm /tmp/verchk-$MODULE.html
-	;;
-groff)
-	wget -q $GNU_URL/$MODULE -O /tmp/verchk-$MODULE.html &&
-	grep -o 'href="'$MODULE'-[0-9]*.*gz"' /tmp/verchk-$MODULE.html |
-		sed 's/href=//g;s/"//g' |sort -V |tail -n1
-	rm /tmp/verchk-$MODULE.html
-	;;
--V|--version)
-	echo "verchk.sh $VERCHK_VERSION" && exit 0
-	;;
-*)
-	echo "Usage: verchk.sh MODULE" && exit 22
-	;;
+autoconf) verchk xz;;
+automake) verchk xz;;
+bc) verchk gz;;
+bison) verchk xz;;
+coreutils) verchk xz;;
+cpio) verchk bz2;;
+ddrescue) verchk lz;;
+diffutils) verchk xz;;
+ed) verchk lz;;
+fdisk) verchk bz2;;
+findutils) verchk gz;;
+fontutils) verchk gz;;
+gawk) verchk xz;;
+gdb) verchk xz;;
+gdbm) verchk gz;;
+gettext) verchk xz;;
+gmp) verchk xz;;
+grep) verchk xz;;
+groff) verchk gz;;
+-V|--version) echo "verchk.sh $VERCHK_VERSION" && exit 0;;
+*) echo "Usage: verchk.sh MODULE" && exit 22;;
 esac
 
