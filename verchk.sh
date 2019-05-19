9000a6a6bf94c338642cef62ea25468df3bcc459
Stephane Fontaine <bsfc1685@gmail.com>
2019-05-19 11:08:14 -0400

    0.0.1.5: Added XORG_URL /data and /util

--- verchk.sh
+++ verchk.sh
@@ -1,69 +1,101 @@
 #!/bin/bash
 
-VERCHK_VERSION="0.0.1.4"
+VERCHK_VERSION="0.0.1.5"
 MODULE="$1"
 GNU_URL="http://gnu.mirror.iweb.com/"
 XORG_APP_URL="https://www.x.org/releases/individual/app/"
+XORG_DATA_URL="https://www.x.org/releases/individual/data/"
+XORG_UTIL_URL="https://www.x.org/releases/individual/util/"
 
 vc_gnu() {
-	wget -q $GNU_URL/$MODULE -O /tmp/verchk-$MODULE.html &&
-	grep -Eo 'href="'$MODULE'-[0-9]*.*(xz|bz2|gz|lz)"' /tmp/verchk-$MODULE.html |
+	wget -q $GNU_URL/$MODULE -O /tmp/verchk-gnu-$MODULE.html &&
+	grep -Eo 'href="'$MODULE'-[0-9]*.*(xz|bz2|gz|lz)"' /tmp/verchk-gnu-$MODULE.html |
 		sed '/latest/d;s/href=//g;s/"//g' |sort -V |tail -n1
-	rm /tmp/verchk-$MODULE.html
+	rm /tmp/verchk-gnu-$MODULE.html
 }
-vc_xorg() {
+vc_xorg_app() {
 	# Only fetch the file once, all modules are on that page ...let a reboot clear the file
 	[ -f /tmp/verchk-xorg-app.html ] || wget -q $XORG_APP_URL -O /tmp/verchk-xorg-app.html &&
 	grep -Eo 'href="'$MODULE'-[0-9]*.*(xz|bz2|gz|lz)"' /tmp/verchk-xorg-app.html |
 		sed '/latest/d;s/href=//g;s/"//g' |sort -V |tail -n1
 #	rm /tmp/verchk-xorg-app.html
 }
+vc_xorg_data() {
+	# Only fetch the file once, all modules are on that page ...let a reboot clear the file
+	[ -f /tmp/verchk-xorg-data.html ] || wget -q $XORG_DATA_URL -O /tmp/verchk-xorg-data.html &&
+	grep -Eo 'href="'$MODULE'-[0-9]*.*(xz|bz2|gz|lz)"' /tmp/verchk-xorg-data.html |
+		sed '/latest/d;s/href=//g;s/"//g' |sort -V |tail -n1
+#	rm /tmp/verchk-xorg-data.html
+}
+vc_xorg_util() {
+	# Only fetch the file once, all modules are on that page ...let a reboot clear the file
+	[ -f /tmp/verchk-xorg-util.html ] || wget -q $XORG_UTIL_URL -O /tmp/verchk-xorg-util.html &&
+	grep -Eo 'href="'$MODULE'-[0-9]*.*(xz|bz2|gz|lz)"' /tmp/verchk-xorg-util.html |
+		sed '/latest/d;s/href=//g;s/"//g' |sort -V |tail -n1
+#	rm /tmp/verchk-xorg-util.html
+}
+
 
 case "$MODULE" in
-appres) vc_xorg;;
+appres) vc_xorg_app;;
 autoconf) vc_gnu;;
 automake) vc_gnu;;
 bc) vc_gnu;;
-bdftopcf) vc_xorg;;
+bdftopcf) vc_xorg_app;;
 bison) vc_gnu;;
-bitmap) vc_xorg;;
+bitmap) vc_xorg_app;;
 coreutils) vc_gnu;;
 cpio) vc_gnu;;
 ddrescue) vc_gnu;;
 diffutils) vc_gnu;;
 ed) vc_gnu;;
-editres) vc_xorg;;
+editres) vc_xorg_app;;
 fdisk) vc_gnu;;
 findutils) vc_gnu;;
 fontutils) vc_gnu;;
 gawk) vc_gnu;;
+gccmakedep) vc_xorg_util;;
 gdb) vc_gnu;;
 gdbm) vc_gnu;;
 gettext) vc_gnu;;
 gmp) vc_gnu;;
 grep) vc_gnu;;
 groff) vc_gnu;;
-iceauth) vc_xorg;;
-ico) vc_xorg;;
-mkfontdir) vc_xorg;;
-mkfontscale) vc_xorg;;
-rgb) vc_xorg;;
-setxkbmap) vc_xorg;;
-showfont) vc_xorg;;
-twm) vc_xorg;;
-viewres) vc_xorg;;
-xauth) vc_xorg;;
-xev) vc_xorg;;
-xinit) vc_xorg;;
-xinput) vc_xorg;;
-xkbcomp) vc_xorg;;
-xkill) vc_xorg;;
-xmessage) vc_xorg;;
-xmodmap) vc_xorg;;
-xrandr) vc_xorg;;
-xrdb) vc_xorg;;
-xset) vc_xorg;;
-xsetroot) vc_xorg;;
+iceauth) vc_xorg_app;;
+ico) vc_xorg_app;;
+imake) vc_xorg_util;;
+makedepend) vc_xorg_util;;
+mkfontdir) vc_xorg_app;;
+mkfontscale) vc_xorg_app;;
+rgb) vc_xorg_app;;
+setxkbmap) vc_xorg_app;;
+showfont) vc_xorg_app;;
+twm) vc_xorg_app;;
+util-macros) vc_xorg_util;;
+viewres) vc_xorg_app;;
+xauth) vc_xorg_app;;
+xbitmaps) vc_xorg_data;;
+xcursor-themes) vc_xorg_data;;
+xev) vc_xorg_app;;
+xinit) vc_xorg_app;;
+xinput) vc_xorg_app;;
+xkbcomp) vc_xorg_app;;
+xkbdata) vc_xorg_data;;
+xkill) vc_xorg_app;;
+xkeyboard-config)
+	wget -q https://www.x.org/releases/individual/data/xkeyboard-config/ \
+			-O /tmp/verchk-xorg-xkeyboard-config.html &&
+	grep -Eo 'href="'$MODULE'-[0-9]*.*(xz|bz2|gz|lz)"' /tmp/verchk-xorg-xkeyboard-config.html |
+		sed '/latest/d;s/href=//g;s/"//g' |sort -V |tail -n1
+	rm /tmp/verchk-xorg-xkeyboard-config.html
+	;;
+xmessage) vc_xorg_app;;
+xmodmap) vc_xorg_app;;
+xorg-cf-files) vc_xorg_util;;
+xrandr) vc_xorg_app;;
+xrdb) vc_xorg_app;;
+xset) vc_xorg_app;;
+xsetroot) vc_xorg_app;;
 -V|--version) echo "verchk.sh $VERCHK_VERSION" && exit 0;;
 *) echo "Usage: verchk.sh MODULE" && exit 22;;
 esac
